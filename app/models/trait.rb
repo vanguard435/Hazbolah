cef3c8f9dc7fe3f434485dc1768a07f9d2733519
vanguard435 <isaacquek@hotmail.com>
2013-11-15 09:30:34 +0000

    Added some styling, TODO// Ajax, Fix Tables not merging when overlap, Make a better profile page, Mmake non-throwupable home page. Put Add Traits into a Javascript popup, Deadline by Sunday. 24 HR HACKATHON LEGGO//

--- app/models/trait.rb
+++ app/models/trait.rb
@@ -1,4 +1,6 @@
 class Trait < ActiveRecord::Base
+validates :name, :uniqueness => true
+after_rollback Trait.create
   has_many :trait_user
   has_many :users, :through => :trait_user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :trait_user
   has_many :traits, :through => :trait_user
+
+  def ctraituserfind
+  	@findtraituser = Trait.find(n.id).trait_user.first.counter
+  end
+
 end
