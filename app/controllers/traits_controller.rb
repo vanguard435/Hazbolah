6211e51ba54d4a4a2bcddfa02c6b526e3f9ad435
Kishe <kishe@UbunKishe.(none)>
2013-11-16 19:33:07 +0800

    Fixed merge issue

--- app/controllers/traits_controller.rb
+++ app/controllers/traits_controller.rb
@@ -3,9 +3,10 @@ class TraitsController < ApplicationController
   end
 
   def create
-  	@trait = Trait.create(trait_params)
-    TraitUser.create(:user_id => current_user.id, :trait_id => @trait.id)
+  	@trait = Trait.find_or_create_by(trait_params)
+    TraitUser.find_or_create_by(:user_id => current_user.id, :trait_id => @trait.id)
     redirect_to dashboard_index_path
+    return false
   end
 
   def new
