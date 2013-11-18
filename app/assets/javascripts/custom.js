ca0c4d447706e2957da06fbafea2117bfc54bfd5
vanguard435 <isaacquek@hotmail.com>
2013-11-18 13:12:25 +0000

    Fixed toggle bug

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
   setTimeout(function(){
     $('.kill').fadeOut("slow");
   }, 2000);
-  	$('#trait_list').click(function(){
+  	$('#trait_list').click(function(e){
+  		if(e.target === this) {
   	  $(".fi-prohibited").fadeToggle();
+  	}
   	})
  })
