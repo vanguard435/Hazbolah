eb81e63c87c849e2520f6c05393ad678e0dc4b9a
vanguard435 <isaacquek@hotmail.com>
2013-11-19 14:20:41 +0000

    Fixed options bug, Added downcase of traits method

--- app/controllers/traits_controller.rb
+++ app/controllers/traits_controller.rb
@@ -6,8 +6,10 @@ class TraitsController < ApplicationController
   end
 
   def create
-  	@trait = Trait.find_or_create_by(trait_params)
-    TraitUser.find_or_create_by(:user_id => current_user.id, :trait_id => @trait.id)
+
+  	@trait = Trait.find_or_create_by(name: params[:trait][:name].downcase)
+
+        TraitUser.find_or_create_by(:user_id => current_user.id, :trait_id => @trait.id)
     redirect_to dashboard_index_path
     return false
   end
